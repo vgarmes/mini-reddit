6f271bae46f061aa605e5ac0368d9f1107768d88
Victor Garcia Mestre <vgmestre@gmail.com>
2021-11-08 12:23:24 +0100

    add graphql decorators

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,16 +1,24 @@
 import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+import { Field, ObjectType } from 'type-graphql';
 
-@Entity()
+@ObjectType() // graphql decorator
+@Entity() // mikro-orm decorator
 export class Post {
+  @Field()
   @PrimaryKey()
   id!: number;
 
+  @Field(() => String)
   @Property({ type: 'date' })
   createdAt = new Date();
 
+  @Field(() => String)
   @Property({ type: 'date', onUpdate: () => new Date() })
   updatedAt = new Date();
 
+  @Field(() => String)
   @Property({ type: 'text' })
   title!: string;
 }
+
+// in order to hide fields from Graphql schema, remove the @Field decorator
