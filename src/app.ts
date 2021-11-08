189153f47c6f6b6f198bd5e8f2227898c69b8fb8
Victor Garcia Mestre <vgmestre@gmail.com>
2021-11-08 12:23:02 +0100

    add migrations and apollo server

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,33 @@
+import 'reflect-metadata';
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
+//import { Post } from './entities/Post';
+import microConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    entities: [],
-    dbName: 'minireddit',
-    type: 'postgresql',
-    debug: !__prod__,
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }), // context available to all resolvers
+  });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(5000, () => {
+    console.log('server listening on port 5000...');
   });
 };
 
