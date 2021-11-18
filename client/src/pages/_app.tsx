a165fc99922029ab2eb6cbd9e3009cc4528ab70c
Victor Garcia Mestre <vgmestre@gmail.com>
2021-11-18 19:42:16 +0100

    add graphql provider

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,19 +1,29 @@
-import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
+import { createClient, Provider } from 'urql';
 
-import theme from '../theme'
+const client = createClient({
+  url: 'http://localhost:5000/graphql',
+  fetchOptions: {
+    credentials: 'include',
+  },
+});
+
+import theme from '../theme';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
-  )
+    <Provider value={client}>
+      <ChakraProvider resetCSS theme={theme}>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: true,
+          }}
+        >
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
