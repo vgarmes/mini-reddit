0a164939d008d72e46d2488230a7485b8896c2ab
Victor Garcia Mestre <vgmestre@gmail.com>
2021-11-21 23:21:55 +0100

    exchange 'me' cache after login/register mutations

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,14 +1,72 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
-import { createClient, Provider } from 'urql';
+import { createClient, dedupExchange, fetchExchange, Provider } from 'urql';
+import { cacheExchange, Cache, QueryInput } from '@urql/exchange-graphcache';
+import theme from '../theme';
+import {
+  LoginMutation,
+  RegisterMutation,
+  MeDocument,
+  MeQuery,
+} from '../generated/graphql';
+
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
 
 const client = createClient({
   url: 'http://localhost:5000/graphql',
   fetchOptions: {
     credentials: 'include',
   },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+          register: (_result, args, cache, info) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
-
-import theme from '../theme';
+// we force urql to re-cache after login and register, otherwise the query "me" returns null
 
 function MyApp({ Component, pageProps }) {
   return (
