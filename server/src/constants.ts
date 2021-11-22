a12597a31fcf6ea1aef46e779af4977e56045388
Victor Garcia Mestre <vgmestre@gmail.com>
2021-11-22 14:27:16 +0100

    create COOKIE_OPTIONS constant

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,1 +1,9 @@
+import { CookieOptions } from 'express-session';
 export const __prod__ = process.env.NODE_ENV === 'production';
+export const COOKIE_NAME = 'qid';
+export const COOKIE_OPTIONS = {
+  maxAge: 1000 * 60 * 60 * 24 * 365,
+  httpOnly: true, // so user cant access cookie from the frontend
+  sameSite: 'none', // 'lax' csrf, set to none in dev env so cookie can be sent to Apollo Studio
+  secure: true, // __prod__ cookie only works in https
+} as CookieOptions;
