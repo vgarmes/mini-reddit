26f31864c01e835b2d4d5858a341cb58decdc2af
Victor Garcia Mestre <vgmestre@gmail.com>
2021-11-22 16:34:30 +0100

    wrap page in urql client with SSR

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,12 +1,23 @@
 import { Navbar } from '../components/Navbar';
 import { createUrqlClient } from '../utils/createUrqlClient';
 import { withUrqlClient } from 'next-urql';
+import { usePostsQuery } from '../generated/graphql';
 
-const Index = () => (
-  <>
-    <Navbar />
-    <div>hello world!</div>
-  </>
-);
+const Index = () => {
+  const [{ data }] = usePostsQuery();
 
-export default withUrqlClient(createUrqlClient)(Index);
+  return (
+    <>
+      <Navbar />
+      <div>hello world!</div>
+      <br />
+      {!data ? (
+        <div>loading...</div>
+      ) : (
+        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+      )}
+    </>
+  );
+};
+
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
