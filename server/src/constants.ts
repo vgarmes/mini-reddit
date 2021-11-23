3269d30ee456180cad909255a58dca0d75fd887c
Victor Garcia Mestre <vgmestre@gmail.com>
2021-11-23 10:41:57 +0100

    changed cookie options

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -4,6 +4,6 @@ export const COOKIE_NAME = 'qid';
 export const COOKIE_OPTIONS = {
   maxAge: 1000 * 60 * 60 * 24 * 365,
   httpOnly: true, // so user cant access cookie from the frontend
-  sameSite: 'none', // 'lax' csrf, set to none in dev env so cookie can be sent to Apollo Studio
-  secure: true, // __prod__ cookie only works in https
+  sameSite: 'lax', // 'lax' csrf, set to none in dev env so cookie can be sent to Apollo Studio
+  secure: __prod__, // __prod__ cookie only works in https
 } as CookieOptions;
