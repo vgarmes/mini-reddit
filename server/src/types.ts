2eb155b0cfbd2d2d8a00f337f4b5962c6ec656ec
Victor Garcia Mestre <vgmestre@gmail.com>
2021-11-24 11:50:44 +0100

    add redis to context

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,8 +1,10 @@
 import { EntityManager, Connection, IDatabaseDriver } from '@mikro-orm/core';
 import { Request, Response } from 'express';
+import { Redis } from 'ioredis';
 
 export type MyContext = {
   em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
   req: Request & { session: { userId: number } };
   res: Response;
+  redis: Redis;
 };
