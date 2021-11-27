87fcbc268840a995c7bc5914deac6952a918c1b6
Victor Garcia Mestre <vgmestre@gmail.com>
2021-11-27 17:28:00 +0100

    added many-to-one rel between users and posts

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -5,8 +5,10 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   BaseEntity,
+  ManyToOne,
 } from 'typeorm';
 import { Field, ObjectType } from 'type-graphql';
+import { User } from './User';
 
 @ObjectType() // graphql decorator
 @Entity() // typeorm decorator
@@ -16,16 +18,31 @@ export class Post extends BaseEntity {
   id!: number;
 
   @Field(() => String)
+  @Column()
+  title!: string;
+
+  @Field()
+  @Column()
+  text!: string;
+
+  @Field()
+  @Column({ type: 'int', default: 0 })
+  points!: number;
+
+  @Field()
+  @Column()
+  creatorId: number;
+
+  @ManyToOne(() => User, (user) => user.posts)
+  creator: User;
+
+  @Field(() => String)
   @CreateDateColumn()
   createdAt: Date;
 
   @Field(() => String)
   @UpdateDateColumn()
   updatedAt: Date;
-
-  @Field(() => String)
-  @Column()
-  title!: string;
 }
 
 // in order to hide fields from Graphql schema, remove the @Field decorator
--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -5,8 +5,10 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   BaseEntity,
+  OneToMany,
 } from 'typeorm';
 import { Field, ObjectType } from 'type-graphql';
+import { Post } from './Post';
 
 @ObjectType() // graphql decorator
 @Entity() // typeorm decorator
@@ -14,14 +16,6 @@ export class User extends BaseEntity {
   @Field()
   @PrimaryGeneratedColumn()
   id!: number;
-
-  @Field(() => String)
-  @CreateDateColumn()
-  createdAt: Date;
-
-  @Field(() => String)
-  @UpdateDateColumn()
-  updatedAt: Date;
 
   @Field(() => String)
   @Column({ unique: true })
@@ -33,6 +27,17 @@ export class User extends BaseEntity {
 
   @Column()
   password!: string;
+
+  @OneToMany(() => Post, (post) => post.creator)
+  posts: Post[];
+
+  @Field(() => String)
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Field(() => String)
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
 
 // in order to hide fields from Graphql schema, remove the @Field decorator
