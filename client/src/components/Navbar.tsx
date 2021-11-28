c68fd64aa1cd450b3a57c7872142612341d86739
Victor Garcia Mestre <vgmestre@gmail.com>
2021-11-28 22:43:49 +0100

    sticky navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -42,7 +42,7 @@ const Navbar: React.FC<NavbarProps> = ({}) => {
   }
 
   return (
-    <Flex bg="tomato" p={4}>
+    <Flex zIndex={1} position="sticky" top={0} bg="tomato" p={4}>
       <Box ml={'auto'}>{body}</Box>
     </Flex>
   );
