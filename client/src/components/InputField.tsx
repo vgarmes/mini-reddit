374cd5c295bb30db4ef9b8bf20c2b044a19a5c74
Victor Garcia Mestre <vgmestre@gmail.com>
2021-11-28 11:24:54 +0100

    accept textarea prop

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -1,33 +1,38 @@
 import React, { InputHTMLAttributes } from 'react';
 import { useField } from 'formik';
 import {
+  ComponentWithAs,
   FormControl,
   FormErrorMessage,
   FormLabel,
   Input,
+  InputProps,
+  Textarea,
+  TextareaProps,
 } from '@chakra-ui/react';
 
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
   name: string;
+  textarea?: boolean;
 };
 
 const InputField: React.FC<InputFieldProps> = ({
   label,
+  textarea,
   size: _,
   ...props
 }) => {
+  let InputOrTextarea = Input;
+  if (textarea) {
+    InputOrTextarea = Textarea as any;
+  }
   const [field, { error }] = useField(props);
 
   return (
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input
-        {...field}
-        {...props}
-        id={field.name}
-        placeholder={props.placeholder}
-      />
+      <InputOrTextarea {...field} {...props} id={field.name} />
       {error && <FormErrorMessage>{error}</FormErrorMessage>}
     </FormControl>
   );
