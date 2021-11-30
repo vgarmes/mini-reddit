0b5d93dc6dd6a3ddcc718e86a0552d1e987b10e2
Victor Garcia Mestre <vgmestre@gmail.com>
2021-11-30 22:31:02 +0100

    invalidate posts cache after creating a post

--- client/src/utils/createUrqlClient.ts
+++ client/src/utils/createUrqlClient.ts
@@ -44,6 +44,16 @@ export const createUrqlClient = (ssrExchange: any) => ({
       },
       updates: {
         Mutation: {
+          createPost: (_result, args, cache, info) => {
+            // invalidates cache for posts so they are fetched again after user posts
+            const allFields = cache.inspectFields('Query');
+            const fieldInfos = allFields.filter(
+              (info) => info.fieldName === 'posts'
+            );
+            fieldInfos.forEach((fi) => {
+              cache.invalidate('Query', 'posts', fi.arguments);
+            });
+          },
           logout: (_result, args, cache, info) => {
             betterUpdateQuery<LogoutMutation, MeQuery>(
               cache,
