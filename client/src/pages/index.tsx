fd31cc11ba3f0a4ef22c57e78e5551eee8221d0d
Victor Garcia Mestre <vgmestre@gmail.com>
2021-12-04 17:58:51 +0100

    use Votes, rearrange post

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -13,10 +13,11 @@ import {
 } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import { useState } from 'react';
+import Votes from '../components/Votes';
 
 const Index = () => {
   const [variables, setVariables] = useState({
-    limit: 10,
+    limit: 15,
     cursor: null as string | null,
   });
   const [{ data, fetching }] = usePostsQuery({
@@ -40,10 +41,14 @@ const Index = () => {
       ) : (
         <Stack spacing={8}>
           {data.posts.posts.map((p) => (
-            <Box key={p.id} p={5} shadow="md" borderWidth="1px">
-              <Heading fontSize="xl">{p.title}</Heading>
-              <Text mt={4}>{p.textSnippet}...</Text>
-            </Box>
+            <Flex key={p.id} p={5} shadow="md" borderWidth="1px" align="center">
+              <Votes post={p} />
+              <Box>
+                <Heading fontSize="xl">{p.title}</Heading>
+                <Text>posted by {p.creator.username}</Text>
+                <Text mt={4}>{p.textSnippet}...</Text>
+              </Box>
+            </Flex>
           ))}
         </Stack>
       )}
