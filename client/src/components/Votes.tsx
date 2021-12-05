43175a5707b3ca934dc7998d90bf48b31cfa7157
Victor Garcia Mestre <vgmestre@gmail.com>
2021-12-05 19:46:25 +0100

    arrow styles depending on vote status

--- client/src/components/Votes.tsx
+++ client/src/components/Votes.tsx
@@ -20,8 +20,12 @@ const Votes = ({ post }: Props) => {
         aria-label="upvote post"
         variant="ghost"
         icon={<ChevronUpIcon boxSize={6} />}
+        colorScheme={post.voteStatus === 1 ? 'green' : undefined}
         isLoading={loadingState === 'upvote-loading'}
         onClick={async () => {
+          if (post.voteStatus === 1) {
+            return;
+          }
           setLoadingState('upvote-loading');
           await vote({
             postId: post.id,
@@ -35,8 +39,12 @@ const Votes = ({ post }: Props) => {
         aria-label="downvote post"
         variant="ghost"
         icon={<ChevronDownIcon boxSize={6} />}
+        colorScheme={post.voteStatus === -1 ? 'red' : undefined}
         isLoading={loadingState === 'downvote-loading'}
         onClick={async () => {
+          if (post.voteStatus === -1) {
+            return;
+          }
           setLoadingState('downvote-loading');
           await vote({
             postId: post.id,
