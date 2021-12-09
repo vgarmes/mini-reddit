c1e4ad2c27db98546e965071cbfaad1d85ca2878
Victor Garcia Mestre <vgmestre@gmail.com>
2021-12-10 00:53:26 +0100

    create component with edit/delete post buttons

---
+++ client/src/components/PostButtons.tsx
@@ -0,0 +1,42 @@
+import { IconButton, Link } from '@chakra-ui/react';
+import { EditIcon, DeleteIcon } from '@chakra-ui/icons';
+import NextLink from 'next/link';
+import { useDeletePostMutation, useMeQuery } from '../generated/graphql';
+
+interface PostButtonsProps {
+  id: number;
+  creatorId: number;
+}
+
+const PostButtons: React.FC<PostButtonsProps> = ({ id, creatorId }) => {
+  const [, deletePost] = useDeletePostMutation();
+  const [{ data: meData }] = useMeQuery();
+
+  if (meData?.me?.id !== creatorId) {
+    return null;
+  }
+
+  return (
+    <>
+      <NextLink href={`/post/edit/${id}`} passHref>
+        <IconButton
+          as={Link}
+          ml="auto"
+          mr={4}
+          aria-label="edit post"
+          icon={<EditIcon />}
+        />
+      </NextLink>
+      <IconButton
+        ml="auto"
+        aria-label="delete post"
+        icon={<DeleteIcon />}
+        onClick={() => {
+          deletePost({ id });
+        }}
+      />
+    </>
+  );
+};
+
+export default PostButtons;
