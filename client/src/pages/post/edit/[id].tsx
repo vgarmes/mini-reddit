f5a8007e1abb31f2629ac4e72d506cda40cd3339
Victor Garcia Mestre <vgmestre@gmail.com>
2021-12-10 00:30:46 +0100

    create page for edit post

---
+++ client/src/pages/post/edit/[id].tsx
@@ -0,0 +1,78 @@
+import { withUrqlClient } from 'next-urql';
+import { createUrqlClient } from '../../../utils/createUrqlClient';
+import Layout from '../../../components/Layout';
+import { Formik, Form } from 'formik';
+import { Box, Button } from '@chakra-ui/react';
+import InputField from '../../../components/InputField';
+import {
+  usePostQuery,
+  useUpdatePostMutation,
+} from '../../../generated/graphql';
+import { useGetPostIdFromUrl } from '../../../hooks/useGetPostIdFromUrl';
+import { useRouter } from 'next/router';
+
+const EditPost = () => {
+  const router = useRouter();
+  const intId = useGetPostIdFromUrl();
+  const [{ data, fetching }] = usePostQuery({
+    pause: intId === -1,
+    variables: {
+      id: intId,
+    },
+  });
+
+  const [, updatePost] = useUpdatePostMutation();
+
+  if (fetching) {
+    return (
+      <Layout>
+        <Box>loading...</Box>
+      </Layout>
+    );
+  }
+
+  if (!data?.post) {
+    return (
+      <Layout>
+        <Box>could not find post</Box>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout variant="small">
+      <Formik
+        initialValues={{ title: data.post.title, text: data.post.text }}
+        onSubmit={async (values) => {
+          await updatePost({ id: intId, ...values });
+          router.back();
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField name="title" placeholder="title" label="Title" />
+            <Box mt={4}>
+              <InputField
+                textarea
+                name="text"
+                placeholder="text..."
+                label="Body"
+              />
+            </Box>
+
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              colorScheme="teal"
+            >
+              update post
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Layout>
+  );
+};
+
+export default withUrqlClient(createUrqlClient)(EditPost);
